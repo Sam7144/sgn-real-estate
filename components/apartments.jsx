eb25c7a1fcff4a25807271e8d1212d093d70565c
Sam7144 <79979572+Sam7144@users.noreply.github.com>
2023-04-19 21:28:49 +0300

    responsive

--- components/apartments.jsx
+++ components/apartments.jsx
@@ -9,7 +9,7 @@ export default function Apartments({data}) {
       <div className='apartment'>
       {data.map((ev)=>(
       <Link key={ev.name} href={`/property/${ev.name}`} legacyBehavior>
-      <a className='e m-1 p-2'>
+      <a className='e m-1 p-2 try'>
       <Image alt={ev.title} width={300} height={300} className='imga' src={ev.image}/>
       <div>
       <p>{ev.name}</p>
--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,12 @@ import Layout from '@/mainlayout/Layout'
 import '@/styles/globals.css'
 import '@/styles/header.scss'
 import '@/styles/hero.scss'
-import '@/styles/apart.scss'
 import '@/styles/footer.scss'
 import '@/styles/about.scss'
 import '@/styles/contact.scss'
 import '@/styles/faq.scss'
 import '@/styles/props.scss'
+import '@/styles/apart.scss'
 export default function App({ Component, pageProps }) {
   return (
     
